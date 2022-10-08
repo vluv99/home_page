dafa2edbe9056fee443717879b0dce3f086e6127
vluv99 <v.luva99@gmail.com>
2022-10-08 19:01:42 +0200

    [WIP] grid view
     - now we have a grid view with draggale elements
     - tho it still needs work since the animation and dransitions are a bit questionable...

--- packages/home/src/main.ts
+++ packages/home/src/main.ts
@@ -1,4 +1,9 @@
 import './app/app.element.ts';
 import './app/background/app.background';
 import './app/layout/app.layout';
+import './app/grid-layout/app.grid-layout';
+import {sortable} from'./app/grid-layout/app.grid-layout';
 
+sortable( document.getElementById('list'), function (item){
+    /* console.log(item); */
+});
