9ebf405b905e758f37f4bf58405e170d56b3ed7c
vluv99 <v.luva99@gmail.com>
2022-10-11 15:05:55 +0200

    icon
     - Added icon element
     - for now its only svg since i couldnt for the ... make fontawesome work with he project yet

--- packages/home/src/main.ts
+++ packages/home/src/main.ts
@@ -1,8 +1,11 @@
+//import "@fortawesome/fontawesome-free";
+
 import './app/app.element.ts';
 import './app/background/app.background';
 import './app/layout/app.layout';
 import './app/grid-layout/app.grid-layout';
 import './app/widgets/weather/widget.weather';
 
-//hello - testing git name change here
+import './app/icon/app-icon';
+
 
