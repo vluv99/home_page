1c0314a72f3ffb3b750aaa306fb49d47a420b7e5
dpeter99 <dpeter99@gmail.com>
2022-10-16 09:57:19 +0200

    Working lit scss import

--- packages/home/webpack.config.js
+++ packages/home/webpack.config.js
@@ -5,8 +5,16 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const stylesHandler = MiniCssExtractPlugin.loader;
 
 module.exports = (config, context) => {
+  const c = config;
 
-  const c = {
+  c.module.rules[3].oneOf.unshift(        {
+    test: /\.scss$|\.sass$/,
+    resourceQuery: "?lit",
+    use: ['lit-scss-loader','extract-loader', "css-loader", "postcss-loader", "sass-loader"],
+  });
+
+  
+  /*{
     ...config,
     module: {
       ...config.module,
@@ -15,16 +23,16 @@ module.exports = (config, context) => {
           test: /\.html$/i,
           use: 'raw-loader',
         },
-        ...config.module.rules,
+        
         {
           test: /\.scss$|\.sass$/,
           resourceQuery: "?lit",
-          use: ['lit-scss-loader'],
+          use: ['lit-scss-loader', 'extract-loader'],
         },
-        
+        ...config.module.rules,
       ],
     },
-  };
+  };*/
 
   console.log(c.module.rules);
   console.log(c.module.rules[3]);
