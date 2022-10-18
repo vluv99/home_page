6426a7f0b1846f754e3bf03646cb87ccf3fbd918
vluv99 <v.luva99@gmail.com>
2022-10-18 09:50:55 +0200

    small addons to xkcd widget

--- packages/home/src/app/widgets/xkcd/widget.xkcd.ts
+++ packages/home/src/app/widgets/xkcd/widget.xkcd.ts
@@ -1,28 +1,36 @@
 import { LitElement, html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
-import {until} from 'lit/directives/until.js';
+import { until } from 'lit/directives/until.js';
 
 import styles from './widget.xkcd.scss?lit';
 
 @customElement('widget-xkcd')
 export class WidgetXkcd extends LitElement {
 
-  
+
   public get json() {
     return this.fetchComic();
   }
-  
-  static styles =  styles;
+
+  static styles = styles;
 
   render() {
     //console.log(this.json)
-    
+
 
     return html`
       <div class="widget--container">
-        <div>XKCD ${until(this.fetchComic().then((d)=>html`${d.num}`), html``)} </div>
-        ${until(this.fetchComic().then((d)=>html`<img src=${d.img}>`), html`<span>Loading...</span>`)}
+        <div>XKCD ${until(this.fetchComic().then((d) => html`${d.num}`), html``)} </div>
+        
+        ${until(this.fetchComic().then((d) => 
+          html`
+          <a target="_blank" href=${d.img}>
+            <img src=${d.img} alt="${d.title}">
+          </a>
+          `), 
+          html`<span>Loading...</span>`)}
+        
       </div>
       `;
   }
@@ -30,7 +38,7 @@ export class WidgetXkcd extends LitElement {
   async fetchComic() {
 
     const res = await fetch('https://xkcd.now.sh/?comic=latest')
-    return res.json();  
+    return res.json();
   }
 }
 
